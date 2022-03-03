70676a09289867dbc9f957de377bf59df53cd8a0
DougSilvera <shortxjdoug@gmail.com>
2022-03-03 15:08:41 -0500

    phase 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace BankHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+            
+            Console.Write("What is your team members name?: ");
+            string teamMember = Console.ReadLine();
+            
+            Console.Write("Enter team member's skill level (number): ");
+            var teamMemberSkill = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your team member's courage level (0.0-2.0): ");
+            var teamMemberCourage = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Team Member information:\nName: {teamMember} \nSkill level: {teamMemberSkill} \nCourage level: {teamMemberCourage}");
+
+
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,10 @@
+
+namespace BankHeist
+{
+    public class TeamMember
+    {
+        public string Name {get; set;}
+        public int SkillLevel {get; set;}
+        public double Courage {get; set;}
+    }
+}
